84734a2367b6ba145c198ed8a6f569f50f0b7b0d
unknown <davidglauber2010@hotmail.com>
2021-03-22 12:07:55 -0300

    Terminada a tela de notificacoes

--- src/screens/notifications/NotificationsA.js
+++ src/screens/notifications/NotificationsA.js
@@ -12,6 +12,7 @@ import {
   StatusBar,
   Image,
   Dimensions,
+  Text,
   TouchableOpacity,
   StyleSheet,
   View
@@ -40,6 +41,19 @@ const windowHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   paddingTitle: {
     padding: 30
+  },
+  title: {
+    marginLeft: windowWidth/6, 
+    fontSize: 15, 
+    fontWeight: 'bold', 
+    color: 'white'
+  }
+  ,
+  titleMain: {
+    marginLeft: 10, 
+    fontSize: 15, 
+    fontWeight: 'bold', 
+    color: 'white'
   }
 })
 
@@ -51,57 +65,7 @@ export default class NotificationsA extends Component {
     super(props);
 
     this.state = {
-      modalizeRef: React.createRef(null),
-      notifications: [
-        {
-          notificationId: 5,
-          type: "delivered",
-          title: "YooHoo! Your order is delivered",
-          text: "We hope you like it! Please send us your feedback and rate.",
-          meta: "just now",
-          readOut: false
-        },
-        {
-          notificationId: 0,
-          type: "failed_delivery",
-          title: "Oops. Failed attempt delivery",
-          text: "You were not available at the time of the first delivery attempt. A second attempt will be made. Please contact us at +1-234-567-8910.",
-          meta: "3 min ago",
-          readOut: false
-        },
-        {
-          notificationId: 1,
-          type: "ready_for_delivery",
-          title: "Your order is out for delivery",
-          text: "We are at your doorstep. Open it to us.",
-          meta: "5 min ago",
-          readOut: false
-        },
-        {
-          notificationId: 2,
-          type: "on_the_way",
-          title: "Your order is on the way",
-          text: "Good news! Your order is arriving today. Expected to be delivered by 6:30 pm.",
-          meta: "21 min ago",
-          readOut: false
-        },
-        {
-          notificationId: 3,
-          type: "exception",
-          title: "Delivery exception",
-          text: "Your order encounters a delivery exception. Please contact us at +1-234-567-8910.",
-          meta: "1 day ago",
-          readOut: true
-        },
-        {
-          notificationId: 4,
-          type: "delivered",
-          title: "YooHoo! Your order is delivered",
-          text: "We hope you like it! Please send us your feedback and rate.",
-          meta: "5 days ago",
-          readOut: true
-        }
-      ]
+      modalizeRef: React.createRef(null)
     };
   }
 
@@ -115,41 +79,21 @@ export default class NotificationsA extends Component {
     navigation.navigate(screen);
   };
 
-  swipeoutOnPressRemove = item => () => {
-    let { notifications } = this.state;
-    const index = notifications.indexOf(item);
-
-    notifications = remove(notifications, n => notifications.indexOf(n) !== index);
-
-    this.setState({
-      notifications
-    });
-  };
-
 
   openModalize() {
     const modalizeRef = this.state.modalizeRef;
     modalizeRef.current?.open()
   }
 
-  keyExtractor = item => item.notificationId.toString();
+  confirmButton() {
+    alert('Serviço confirmado! Lembre-se de comparecer no local e ativar o modo pagamento no final do serviço!')
+  }
 
-  renderItem = ({ item, index }) => (
-    <NotificationItem
-      activeOpacity={0.85}
-      type={item.type}
-      title={item.title}
-      text={item.text}
-      meta={item.meta}
-      // onPress={this.navigateTo("")}
-      readOut={item.readOut}
-      swipeoutOnPressRemove={this.swipeoutOnPressRemove(item)}
-    />
-  );
+  deniedButton() {
+    alert('Serviço cancelado! O usuário contratante será informado sobre o cancelamento')
+  }
 
   render() {
-    const { notifications } = this.state;
-
     return (
       <SafeBackground>
         <StatusBar
@@ -159,29 +103,14 @@ export default class NotificationsA extends Component {
 
         <View>
           <Heading style={styles.paddingTitle}>Notificações</Heading>
-          <View style={{width: windowWidth/1.06, height:100, backgroundColor:'#d98b0d', flexDirection:'row', borderRadius:10, marginTop:20, marginLeft:10, marginRight:10, alignItems:'center'}}>
+          <View style={{width: windowWidth/1.06, height:100, backgroundColor: this.context.dark ? '#3F3F3F' : '#d98b0d', flexDirection:'row', borderRadius:10, marginTop:20, marginLeft:10, marginRight:10, alignItems:'center'}}>
             <Image source={{uri: 'https://veja.abril.com.br/wp-content/uploads/2021/01/GettyImages-1229893385.jpg.jpg'}} style={{height:54, width:54, marginLeft:20, borderRadius:20}}/>
-            <Title style={{marginLeft:10}}>Rodrigo Lombardi</Title>
-              <TouchableOpacity onPress={() => this.openModalize()} style={{width:30, height:30, borderRadius: 20, position:'absolute', right: windowWidth/11, backgroundColor: 'white', justifyContent:'center', alignItems:'center'}}>
+            <Text  style={styles.titleMain}>Rodrigo Lombardi</Text>
+              <TouchableOpacity onPress={() => this.openModalize()} style={{width:30, height:30, borderRadius: 20, position:'absolute', right: windowWidth/11, backgroundColor: this.context.dark ? '#3F3F3F': 'white', justifyContent:'center', alignItems:'center'}}>
                 <IconResponsiveNOBACK name="at" size={24}/>
               </TouchableOpacity>
           </View>
 
-          <View style={{width: windowWidth/1.06, height:100, backgroundColor:'#d98b0d', flexDirection:'row', borderRadius:10, marginTop:20, marginLeft:10, marginRight:10, alignItems:'center'}}>
-            <Image source={{uri: 'https://veja.abril.com.br/wp-content/uploads/2021/01/GettyImages-1229893385.jpg.jpg'}} style={{height:54, width:54, marginLeft:20, borderRadius:20}}/>
-            <Title style={{marginLeft:10}}>Rodrigo</Title>
-              <TouchableOpacity onPress={() => this.openModalize()} style={{width:30, height:30, borderRadius: 20, position:'absolute', right: windowWidth/11, backgroundColor: 'white', justifyContent:'center', alignItems:'center'}}>
-                <IconResponsiveNOBACK name="at" size={24}/>
-              </TouchableOpacity>
-          </View>
-
-          <View style={{width: windowWidth/1.06, height:100, backgroundColor:'#d98b0d', flexDirection:'row', borderRadius:10, marginTop:20, marginLeft:10, marginRight:10, alignItems:'center'}}>
-            <Image source={{uri: 'https://veja.abril.com.br/wp-content/uploads/2021/01/GettyImages-1229893385.jpg.jpg'}} style={{height:54, width:54, marginLeft:20, borderRadius:20}}/>
-            <Title style={{marginLeft:10}}>Rodrigo Lombardi</Title>
-              <TouchableOpacity onPress={() => this.openModalize()} style={{width:30, height:30, borderRadius: 20, position:'absolute', right: windowWidth/11, backgroundColor: 'white', justifyContent:'center', alignItems:'center'}}>
-                <IconResponsiveNOBACK name="at" size={24}/>
-              </TouchableOpacity>
-          </View>
         </View>
 
         {/*Modalize dos comentários*/}
@@ -190,15 +119,48 @@ export default class NotificationsA extends Component {
             snapPoint={500}
             modalStyle={this.context.dark ? {backgroundColor:'#3E3C3F'} : {backgroundColor:'#fff'}}
           >
-          <View style={{width: windowWidth/1.06, height:100, backgroundColor:'#d98b0d', flexDirection:'row', borderRadius:10, marginTop:20, marginLeft:10, marginRight:10, alignItems:'center'}}>
+          <View style={{width: windowWidth/1.06, height:100, backgroundColor: '#d98b0d', flexDirection:'row', borderRadius:10, marginTop:20, marginLeft:10, marginRight:10, alignItems:'center'}}>
             <Image source={{uri: 'https://veja.abril.com.br/wp-content/uploads/2021/01/GettyImages-1229893385.jpg.jpg'}} style={{height:54, width:54, marginLeft:20, borderRadius:20}}/>
-              <Title style={{marginLeft: windowWidth/6, fontSize: 15}}>Rodrigo Lombardi</Title>
+              <Text  style={styles.title}>Rodrigo Lombardi</Text>
           </View>
 
           <View style={{width: windowWidth/1.06, height:500, backgroundColor:'#d98b0d', flexDirection:'row', borderRadius:10, marginTop:20, marginLeft:10, marginRight:10}}>
             <View style={{marginTop:20}}>
-              <Title style={{marginLeft: 30, fontSize: 27}}>CEP: 57046-503</Title>
-              <Title style={{marginLeft: 30, fontSize: 20, marginTop:10}}>SERVIÇO: Caminhão de Mudança</Title>
+              <Title style={{marginLeft: 30, fontSize: 27, color: this.context.dark ? 'white' : 'white'}}>CEP: 57046-503</Title>
+              
+              <View style={{marginLeft: 30, marginTop:30, flexDirection:'row'}}>
+                <IconResponsive name="tools" size={24}/>
+                <Title style={{marginLeft: 20, fontSize: 15, marginTop:5, color: this.context.dark ? 'white' : 'white'}}>Caminhão de Mudança</Title>
+              </View>
+
+              <View style={{marginLeft: 30, marginTop:10, flexDirection:'row'}}>
+                <IconResponsive name="dollar-sign" size={24}/>
+                <Title style={{marginLeft: 27, fontSize: 15, marginTop:5, color: this.context.dark ? 'white' : 'white'}}>R$ 340</Title>
+              </View>
+
+              <View style={{marginLeft: 30, marginTop:10, flexDirection:'row'}}>
+                <IconResponsive name="mobile" size={24}/>
+                <Title style={{marginLeft: 24, fontSize: 15, color: this.context.dark ? 'white' : 'white'}}>(82) 99432-4542</Title>
+              </View>
+              
+
+              <View style={{marginLeft: 30, marginTop:10, flexDirection:'row'}}>
+                <IconResponsive name="calendar-week" size={24}/>
+                <Title style={{marginLeft: 20, fontSize: 15, marginTop:5, color: this.context.dark ? 'white' : 'white'}}>26/07/2021</Title>
+              </View>
+
+
+              <View style={{flexDirection:'row'}}>
+                <TouchableOpacity onPress={() => this.confirmButton()} style={{marginLeft: 30, marginTop:60, flexDirection:'row', padding:10, backgroundColor: 'white', marginRight:20, borderRadius:50}}>
+                  <IconResponsiveNOBACK name="check" size={24}/>
+                  <Title style={{marginLeft: 20, fontSize: 15, marginTop:2, color:'black'}}>Confirmar</Title>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => this.deniedButton()} style={{marginLeft: 30, marginTop:60, flexDirection:'row', padding:10, backgroundColor: 'white', marginRight:120, borderRadius:50}}>
+                  <IconResponsiveNOBACK name="times" size={24}/>
+                  <Title style={{marginLeft: 20, fontSize: 15, marginTop:2, color:'black'}}>Negar</Title>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </Modalize>
